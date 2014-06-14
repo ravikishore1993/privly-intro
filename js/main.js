276ee18ded3bfcc0cdfbd666880b402f072a6675
ravikishore1993 <ravikishore1993@gmail.com>
2014-06-14 17:37:13 +0530

    Adds all decided sections and implements functionality for input

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('#showcaseInputSubmit').click(function(event){
           event.preventDefault();
-          $('#showcaseOutputElement').text($('#showcaseInputText').val());
-    });    
+          $('#showcaseOutputElement').html($('#showcaseInputText').val());
+    });   
+    $('#landing').css('min-height', $(window).height() + 'px'); 
+    $('#showcaseMain').css('width', (parseInt($(window).width()) - 400) + 'px'); 
 });
