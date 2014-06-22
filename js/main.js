e5e1f8bb3368496ee8760c3e356a3d86f6dbcf8a
ravikishore1993 <ravikishore1993@gmail.com>
2014-06-22 20:33:18 +0530

    Adds further 3 steps, text and edit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
+ //   $('#postedTextContent').hide();
     $('#showcaseInputSubmit').click(function(event){
           event.preventDefault();
           $('#showcaseOutputElement').html($('#showcaseInputText').val());
     });   
     $('#landing').css('min-height', $(window).height() + 'px'); 
     $('#showcaseMain').css('width', (parseInt($(window).width()) - 400) + 'px'); 
+
+    $('#showcaseTextSubmit').click(function(){
+    	if($('#inputTextArea').val().length > 0) {
+			$('#postedTextContent').text($('#inputTextArea').val());
+			$('#postedTextContent').fadeIn();
+   			$('#editContent').text($('#inputTextArea').val());
+    	}
+    	
+    });
+
 });
